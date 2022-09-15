509c182424f4afa0ecd59429c0b26f4d47c069ec
Goojoe <goojoer@gmail.com>
2022-09-15 14:37:52 +0800

    字体下载

---
+++ windows/github/fontdonwload.py
@@ -0,0 +1,15 @@
+from github_dl import *
+
+# opposans
+http_dl("opposans.zip","https://static01.coloros.com/www/public/img/topic7/font-opposans.zip?242")
+
+# fira code
+http_dl("FiraCode.zip","https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip")
+
+# firamono
+http_dl("FiraMono.zip","https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraMono.zip")
+# Meslo
+http_dl("Meslo.zip","https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip")
+
+# 思源黑体
+github_dl("adobe-fonts/source-han-sans",1,"source-han-sans-vf.zip")
