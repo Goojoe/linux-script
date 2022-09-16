167b16493c344eebb8fc1f713454fb8fc43ab0f4
Goojoe <goojoer@gmail.com>
2022-09-16 15:38:25 +0800

    update

--- ubuntu/rclone.sh
+++ ubuntu/rclone.sh
@@ -1,5 +1,5 @@
 echo "=====Rclone Install====="
-axel -a -n 3 https://download.nuaa.cf/rclone/rclone/releases/download/v1.59.1/rclone-v1.59.1-linux-amd64.deb
-dpkg -i rclone-v1.59.1-linux-amd64.deb
+axel -a -n 3 https://github.com/rclone/rclone/releases/download/v1.59.2/rclone-v1.59.2-linux-amd64.deb
+dpkg -i rclone-v1.59.2-linux-amd64.deb
 rclone version
 echo "=====Rclone安装完成====="
