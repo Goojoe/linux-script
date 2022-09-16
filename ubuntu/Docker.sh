43caa0c6ecd003643a9150af47307488e45637f3
Goojoe <goojoer@gmail.com>
2022-09-16 13:02:09 +0800

    update

--- ubuntu/Docker.sh
+++ ubuntu/Docker.sh
@@ -1,7 +1,7 @@
 echo "=====Docker Install====="
 axel -a -n 64 "https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-ce-cli_20.10.18~3-0~ubuntu-jammy_amd64.deb"
 
-apt install docker-ce-cli_20.10.18~3-0~ubuntu-jammy_amd64.deb -y
+dpkg -i docker-ce-cli_20.10.18~3-0~ubuntu-jammy_amd64.deb
 echo "启动Docker"
 systemctl start docker
 echo "Docker开机自启"
