dfbdc06c12e5715b57e3262fca7e91968e3c030e
咕咕乔 <goojoer@gmail.com>
2022-10-13 16:22:56 +0800

    Update v2raya.sh

--- ubuntu/v2raya.sh
+++ ubuntu/v2raya.sh
@@ -2,13 +2,13 @@
 apt install net-tools xsel
 ip=`ip a | grep '192.168.*.*' | cut -c10-23`
 port=":2017"
-echo "安装v2ray内核"
-curl -Ls https://mirrors.v2raya.org/go.sh | sudo bash
+echo "安装xray内核"
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 echo "启动服务"
 sudo systemctl start v2raya.service
 sudo systemctl enable v2raya.service
 
-aria2c https://gh.ddlc.top/https://github.com/v2rayA/v2rayA/releases/download/v1.5.9.1698.1/installer_debian_amd64_1.5.9.1698.1.deb
+wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v1.5.9.1698.1/installer_debian_arm64_1.5.9.1698.1.deb
 apt install ./installer_debian_amd64_1.5.9.1698.1.deb
 sudo systemctl start v2raya.service
 sudo systemctl enable v2raya.service
