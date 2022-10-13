d7e2e7739175b38d29d6646a27db3437e3f17d29
咕咕乔 <goojoer@gmail.com>
2022-10-13 16:25:17 +0800

    Update v2raya.sh

--- ubuntu/v2raya.sh
+++ ubuntu/v2raya.sh
@@ -5,8 +5,8 @@ port=":2017"
 echo "安装xray内核"
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 echo "启动服务"
-sudo systemctl start v2raya.service
-sudo systemctl enable v2raya.service
+sudo systemctl start xray.service
+sudo systemctl enable xray.service
 
 wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v1.5.9.1698.1/installer_debian_arm64_1.5.9.1698.1.deb
 apt install ./installer_debian_amd64_1.5.9.1698.1.deb
